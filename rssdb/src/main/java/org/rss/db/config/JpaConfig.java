dc275b2a93c6444bcc1cdc0e7e8d08a6de627e69
abarhub <abarhub@github.com>
2016-09-24 13:49:50 +0200

    correction annotation deprecated

--- rssdb/src/main/java/org/rss/db/config/JpaConfig.java
+++ rssdb/src/main/java/org/rss/db/config/JpaConfig.java
@@ -3,7 +3,7 @@ package org.rss.db.config;
 import org.rss.db.dao.jpa.FeedsGenericJpa;
 import org.rss.db.dao.jpa.UrlJpa;
 import org.rss.db.dao.repository.UrlRepository;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
